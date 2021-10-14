d47b0a4f82a3b4e8e5abdc61c30a185575caaf92
CryptoLeo-3000 <padavekartik@gmail.com>
2021-10-14 10:12:00 +0530

    Added GUI feature

--- swaglyrics/__main__.py
+++ swaglyrics/__main__.py
@@ -2,10 +2,9 @@ import argparse
 import os
 import sys
 import time
-
+import tkinter
 import requests
 from SwSpotify import spotify, SpotifyNotRunning
-
 from swaglyrics import unsupported_txt, SameSongPlaying, __version__ as version, backend_url, api_timeout
 from swaglyrics.cli import lyrics, clear
 from swaglyrics.tab import app
@@ -50,28 +49,23 @@ def unsupported_precheck(force: bool = False) -> None:
             print("You should install SwagLyrics as --user or use sudo to access unsupported.txt.", e)
             sys.exit(1)
 
-
-def show_tab() -> None:
-    from threading import Timer
-    from webbrowser import open
-    print('Firing up a browser tab!')
-    app.template_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
-    app.static_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')
-    port = 5042  # random
-    url = f"http://127.0.0.1:{port}"
-    Timer(1.25, open, args=[url]).start()
-    app.run(port=port)
-
-
 def show_cli(make_issue: bool = False) -> None:
+
     try:
+        song_lyrics = tkinter.Tk()
         song, artist = spotify.current()  # get currently playing song, artist
         print(lyrics(song, artist, make_issue))
-        print('\n(Press Ctrl+C to quit)')
+        words = tkinter.Message(song_lyrics, text=lyrics(song, artist, make_issue))
+        words.pack()
+        song_lyrics.mainloop()
+
     except SpotifyNotRunning as e:
-        print(e)
-        print('\n(Press Ctrl+C to quit)')
+        song_lyrics = tkinter.Tk()
+        words = tkinter.Message(song_lyrics, text=e)
+        words.pack()
+        song_lyrics.mainloop()
         song, artist = None, None
+    
     while True:
         # refresh every 5s to check whether song changed
         # if changed, display the new lyrics
@@ -81,47 +75,12 @@ def show_cli(make_issue: bool = False) -> None:
                     raise SameSongPlaying
                 else:
                     song, artist = spotify.current()
-                    clear()
-                    print(lyrics(song, artist, make_issue))
-                    print('\n(Press Ctrl+C to quit)')
+                    words = tkinter.Message(song_lyrics, text=lyrics(song, artist, make_issue))
+                    words.pack()
+                    song_lyrics.mainloop()
             except (SpotifyNotRunning, SameSongPlaying):
                 time.sleep(5)
         except KeyboardInterrupt:
-            print('\nSure boss, exiting.')
             sys.exit()
 
-
-def main() -> None:
-    # print(r"""
-    #  ____                     _               _
-    # / ___|_      ____ _  __ _| |   _   _ _ __(_) ___ ___
-    # \___ \ \ /\ / / _` |/ _` | |  | | | | '__| |/ __/ __|
-    #  ___) \ V  V / (_| | (_| | |__| |_| | |  | | (__\__ \
-    # |____/ \_/\_/ \__,_|\__, |_____\__, |_|  |_|\___|___/
-    #                     |___/      |___/
-    # 	""")
-    # print('\n')
-
-    parser = argparse.ArgumentParser(
-        description="Get lyrics for the currently playing song on Spotify. Either --tab or --cli is required.")
-
-    parser.add_argument('-t', '--tab', action='store_true', help='Display lyrics in a browser tab.')
-    parser.add_argument('-c', '--cli', action='store_true', help='Display lyrics in the command-line.')
-    parser.add_argument('-n', '--no-issue', action='store_false', help='Disable issue-making on cli.')
-    parser.add_argument('-u', '--update-check', action='store_true', help='Force check for updates.')
-    args = parser.parse_args()
-
-    if args.tab:
-        unsupported_precheck(args.update_check)
-        show_tab()
-
-    elif args.cli:
-        unsupported_precheck(args.update_check)
-        make_issue = args.no_issue
-        show_cli(make_issue)
-    else:
-        parser.print_help()
-
-
-if __name__ == '__main__':
-    main()
+show_cli(True)
